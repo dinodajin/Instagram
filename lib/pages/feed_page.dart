ddb2a4fd1e325b75f9a658109e868a50f3dc0808
djbrachio <djbrachio@gmail.com>
2024-11-04 07:44:31 +0000

    Update feed page

--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram/pages/login_page.dart';
 
 class FeedPage extends StatelessWidget {
   const FeedPage({super.key});
@@ -19,6 +21,20 @@ class FeedPage extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(onPressed: () async {
+        // 로그아웃 처리
+        await FirebaseAuth.instance.signOut();
+
+        // 첫 페이지로 이동 (로그인 페이지)
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return LoginPage();
+            },
+          ),
+        );
+      }),
     );
   }
 }
